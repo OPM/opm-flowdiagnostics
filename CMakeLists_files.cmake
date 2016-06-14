f890ecf445b50700fcf041677045daeb429a8b5c
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2016-06-14 21:15:59 +0200

    Implement First Cut at Notion of Cell Connectivity Graph

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -27,6 +27,7 @@ list (APPEND MAIN_SOURCE_FILES
 	opm/flowdiagnostics/CellSet.cpp
 	opm/flowdiagnostics/CellSetValues.cpp
 	opm/flowdiagnostics/ConnectionValues.cpp
+	opm/flowdiagnostics/ConnectivityGraph.cpp
 	opm/flowdiagnostics/reorder/tarjan.c
 	)
 
@@ -34,6 +35,7 @@ list (APPEND TEST_SOURCE_FILES
 	tests/test_cellset.cpp
 	tests/test_cellsetvalues.cpp
 	tests/test_connectionvalues.cpp
+	tests/test_connectivitygraph.cpp
 	tests/test_tarjan.cpp
 	)
 
@@ -41,4 +43,5 @@ list (APPEND PUBLIC_HEADER_FILES
 	opm/flowdiagnostics/CellSet.hpp
 	opm/flowdiagnostics/CellSetValues.hpp
 	opm/flowdiagnostics/ConnectionValues.hpp
+	opm/flowdiagnostics/ConnectivityGraph.hpp
 	)
